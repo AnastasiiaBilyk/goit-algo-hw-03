a018b6a7b751a8ec420c7473d4c37514201f4f40
AnastasiiaBilyk <nasbilikk@gmail.com>
2024-04-12 18:06:27 +0300

    second commit

--- 3.1.py
+++ 3.1.py
@@ -3,11 +3,13 @@ from datetime import datetime
 date_today = datetime.today()
 
 def get_days_from_today(date):
-   try:
-       date_formatted = datetime.strptime(date, "%Y-%m-%d")
-       diff_date = date_today - date_formatted
-       print(diff_date.days)
-   except:
-       print('Некоректний формат дати. Введіть дату у форматі yyyy-mm-dd:')
 
-get_days_from_today("2025-3-1")
+    try:
+        date_formatted = datetime.strptime(date, "%Y-%m-%d")
+        diff_date = (date_today - date_formatted).days
+        return diff_date
+    except:
+        print('Некоректний формат дати. Введіть дату у форматі yyyy-mm-dd:')
+
+result = get_days_from_today("2025-04-12")
+print(result)
--- 3.2.py
+++ 3.2.py
@@ -1,12 +1,14 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-   if min >= 1 and max <= 1000:
-       lottery_numbers = random.sample(range(min, max+1), quantity)
-       lottery_numbers.sort()
+    
+   if 1 <= min <= max < 1000 and quantity < max - min:
+      lottery_numbers = random.sample(range(min, max+1), quantity)
+      lottery_numbers.sort()
    else:         
-       lottery_numbers = []        
+      lottery_numbers = []        
+   
+   return lottery_numbers
 
-   print(lottery_numbers)
-
-get_numbers_ticket(1,49,6)
+result = get_numbers_ticket(10,6,6)
+print(result)
